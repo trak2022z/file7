1d3ba26db4976299575751c78dbd14f8f2819049
trak2022z <trak2022z@gmail.com>
2022-04-09 21:01:04 +0200

    c2

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 const { Pool} = require('pg')
 
 const pool = new Pool({
-  user: 'ipeiuorjzlmmmv',
-  database: 'd3tvufg06o8ppv',
-  password: '6d0960562e1855471ce935aeba0660a548d13f0ea09df8ac720dff54ee7c72b3',
+  user: 'ljufpvmxjrwujz',
+  database: 'daijjf8q17jq1i',
+  password: '6660457470030b4fdeb37b2e48df627f4d6db20e3cfe4da1c663db48647b6bdb',
   port: 5432,
-  host: 'ec2-52-54-212-232.compute-1.amazonaws.com',
-  ssl: true
+  host: 'ec2-44-194-92-192.compute-1.amazonaws.com',
+  //ssl: true
 })
 
 module.exports = { pool };
